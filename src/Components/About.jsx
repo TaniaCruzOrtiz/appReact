7b28a888a9ac80ccec8b6552e348e29b81d9f5a3
TaniaCO <taniacruzortiz@gmail.com>
2023-10-04 09:27:46 -0600

    Primera version

---
+++ src/Components/About.jsx
@@ -0,0 +1,26 @@
+
+import './About.css';
+
+
+
+const About = () => (
+        <>
+
+        <div className='about--container'>
+            <img src="" alt="imagen" />
+            <h2 className='about--name'>Name</h2>
+            <p className= 'email'>Email</p>
+            <p className='about--info'>Información Personal Lorem ipsum dolor sit amet consecte</p>
+            
+        </div>
+
+        </>
+    )
+    
+
+
+
+
+
+
+export default About;
